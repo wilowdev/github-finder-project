8aaf46a5e21ccac8c17a8f1b07d8a7c91c010439
wilowdev <patryk.wierzba.pw@gmail.com>
2022-03-25 21:31:36 +0100

    user items

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Spinner from '../layouts/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -11,7 +12,7 @@ function UserResults() {
   const fetchUsers = async () => {
     const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        // Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
       },
     });
 
@@ -24,7 +25,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
