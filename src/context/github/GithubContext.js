c2b8f36d429ee7fb4e77fd12770a2d5a58989f9f
wilowdev <patryk.wierzba.pw@gmail.com>
2022-03-31 21:33:33 +0200

    cleaning up

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -4,16 +4,18 @@ import githubReducer from './GithubReducer';
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    leading: true,
+    loading: false,
   };
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  //for testing purposes
   const fetchUsers = async () => {
+    setLoading();
     const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
         // Authorization: `token ${GITHUB_TOKEN}`,
@@ -25,6 +27,12 @@ export const GithubProvider = ({ children }) => {
     dispatch({
       type: 'GET_USERS',
       payload: data,
+    });
+  };
+
+  const setLoading = () => {
+    dispatch({
+      type: 'SET_LOADING',
     });
   };
 
